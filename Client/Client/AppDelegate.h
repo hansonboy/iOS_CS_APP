a04dec1b4c205f9c2177d646736223a7792a19de
libmooc <libmooc@tushuguandeiMac-2.local>
2015-07-12 19:38:48 +0800

    Client iOS by Mac.UESTC.Library

---
+++ Client/Client/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Client
+//
+//  Created by libmooc on 15/7/12.
+//  Copyright (c) 2015年 hansonboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Client/Client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Client
+//
+//  Created by libmooc on 15/7/12.
+//  Copyright (c) 2015年 hansonboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
