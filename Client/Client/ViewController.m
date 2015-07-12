f5d8558563269f5fd76a475b084c932371ee1146
libmooc <libmooc@tushuguandeiMac-2.local>
2015-07-12 21:40:30 +0800

    NSURL  simple  connection - handle data

--- Client/Client/ViewController.m
+++ Client/Client/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
-
+#define ScreenBounds ([UIScreen mainScreen].bounds)
+#define NextYFor(a)   (a.frame.origin.y+a.frame.size.height+10)
 @interface ViewController ()
-
+@property (strong,nonatomic) UILabel * helloworld;
+@property (strong,nonatomic) UIButton * sendButton;
 @end
 
 @implementation ViewController
@@ -17,8 +19,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
 
+    self.sendButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.sendButton.frame = CGRectMake(self.view.center.x-ScreenBounds.size.width/4, 64, ScreenBounds.size.width/2, 30);
+    self.sendButton.backgroundColor = [UIColor blackColor];
+    self.sendButton.alpha = 0.5;
+//    self.sendButton.titleLabel.textColor = [UIColor whiteColor];
+    [self.sendButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.sendButton setTitle:@"请求数据" forState:UIControlStateNormal];
+    [self.sendButton.titleLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.sendButton addTarget:self action:@selector(sendHttps:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.sendButton];
+    
+    self.helloworld = [[UILabel alloc]initWithFrame:CGRectMake(10, NextYFor(self.sendButton), 320, 40)];
+    self.helloworld.textAlignment = NSTextAlignmentCenter;
+    self.helloworld.text = @"我将显示服务器数据";
+    [self.view addSubview:self.helloworld];
+    
+}
+-(void)sendHttps:(UIButton*)sender{
+    NSString * urlStr = @"http://113.54.158.230:8080/iOS_HttpServer_MySQL_Tomcat_JSON/HttpServer";
+    NSURL * url = [NSURL URLWithString:urlStr];
+    NSURLRequest * request = [NSURLRequest requestWithURL:url];
+    NSData * data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSLog(@"-----%d-------",data.length);
+    NSString *rcvData = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    self.helloworld.text = rcvData;
+    NSLog(@"%@",rcvData);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
