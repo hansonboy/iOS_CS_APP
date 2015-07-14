95bad805c4b36886c466640cbaaf96dc8be7c7ef
libmooc <libmooc@tushuguandeiMac-2.local>
2015-07-14 10:22:18 +0800

    Transfer between json object and json string

--- Client/Client/ViewController.m
+++ Client/Client/ViewController.m
@@ -37,7 +37,7 @@
     
 }
 -(void)sendHttps:(UIButton*)sender{
-    NSString * urlStr = @"http://192.168.1.109:8080/iOS_HttpServer_MySQL_Tomcat_JSON/HttpServer?id=1001";
+    NSString * urlStr = @"http://113.54.158.225:8080/iOS_HttpServer_MySQL_Tomcat_JSON/HttpServer?id=1001";
     NSURL * url = [NSURL URLWithString:urlStr];
     NSURLRequest * request = [NSURLRequest requestWithURL:url];
     NSData * data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
@@ -45,6 +45,55 @@
     NSString *rcvData = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
     self.helloworld.text = rcvData;
     NSLog(@"%@",rcvData);
+    
+    NSError * __autoreleasing error;
+    NSDictionary * resultJson = [[NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error] objectForKey:@"Person"];
+    if (resultJson == nil) {
+        NSLog(@"JSON 解析失败");
+    }
+    NSLog(@"id = %@,name = %@,age = %@",[resultJson objectForKey:@"id"],[resultJson objectForKey:@"name"],[resultJson objectForKey:@"age"]);
+    
+    NSDictionary * dic = [NSDictionary dictionaryWithObjectsAndKeys:@"1001",@"id",@"wangjiawnei",@"name",nil];
+//    if([NSJSONSerialization isValidJSONObject:dic]){
+//        NSError *error;
+//        NSData * resultData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&error];
+//        NSLog(@"Json data:%@",[[NSString alloc]initWithData:resultData encoding:NSUTF8StringEncoding]);
+//    }
+    NSString *jsonString = [self jsonString:[self JSONData:dic]];
+    NSLog(@"jsonString:%@",jsonString);
+    NSLog(@"jsonObject:%@",[self JSONObject:[self jsonData:jsonString]]);
+    
+    
+    
+}
+//将NSDictionary 和 NSArray 转化为 JSON data
+-(NSData*)JSONData:(id)object{
+    NSError * error;
+    if ([NSJSONSerialization isValidJSONObject:object]) {
+        NSData * jsonData = [NSJSONSerialization dataWithJSONObject:object options:NSJSONWritingPrettyPrinted error:&error];
+        if([jsonData length]>0 && error == nil){
+            return jsonData;
+        }else{
+            return nil;
+        }
+    }else return nil;
+}
+-(NSString*)jsonString:(NSData*)jsonData{
+    return [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+//将JSonData 转化为NSArray 或者 NSDictionary
+-(NSData*)jsonData:(NSString*)jsonString{
+    return [jsonString dataUsingEncoding:NSASCIIStringEncoding];
+}
+-(id)JSONObject:(NSData*)jsonData{
+    NSError*error = nil;
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:&error];
+    if(jsonObject != nil&& error ==nil) {
+        return jsonObject;
+    }else{
+        //解析错误
+        return nil;
+    }
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
